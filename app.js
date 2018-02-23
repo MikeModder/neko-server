14c4378393595b64a27974c8ababd8b5f3236dd6
MikeModder <15313620+MikeModder@users.noreply.github.com>
2018-02-23 13:16:33 -0500

    heroku v2

--- app.js
+++ app.js
@@ -2,28 +2,63 @@ const express = require('express');
 const RedWire = require('redwire');
 const morgan = require('morgan');
 const enmap = require('enmap');
+const moment = require('moment');
+const bodyParse = require('body-parser');
+
+const config = require('./config.json');
 
 const proxy = new RedWire({
     http: {
-        port: 8092
+        port: config.ports.proxy
     }
 });
 const app = express();
 app.locals.configs = new enmap();
 
-//redbird.register('http://hpmobile.jp/', 'http://192.168.0.9:8080/');
-proxy.http('hpmobile.jp', 'localhost:8080');
+app.locals.newCfg = (name) => {
+    //Add the new config
+    app.locals.configs.set(name, { fish: { gold: 1, silver: 20 }, pass: 'Default', date: moment().format('YYYY-MM-DD') });
+    //Then delete it after 5 minutes
+    setTimeout(() => {
+        app.locals.configs.delete(name);
+    }, 5 * 60000);
+};
+
+proxy.http('hpmobile.jp', config.host);
 
 app.use(morgan('dev'));
+app.use(bodyParse.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', `${__dirname}/views`);
+
+app.use((req, res, next) => {
+    let hascfg = req.app.locals.configs.has(req.ip);
+    if(!hascfg) req.app.locals.newCfg(req.ip);
+    req.cfg = req.app.locals.configs.get(req.ip);
+    next();
+});
 
 let neko = require('./neko.js');
 app.use('/app/nekoatsume', neko);
 
 app.get('/', (req, res) => {
-    res.send('Control panel will be here!');
+    res.render('config', { config: req.cfg });
 });
 
-app.listen(8080, () => {
-    console.log(`[INFO] Server ready and listening on port 8080!`);
+app.post('/update', (req, res) => {
+    console.log(req.body);
+    let gold = req.body.gold;
+    let silver = req.body.silver;
+    let date = req.body.date;
+    let pass = req.body.pass;
+    if(!gold || !silver || !date || !pass) return res.send(`Missing one or more fields! Try again!`);
+    app.locals.configs.set(req.ip, { fish: { gold: gold, silver: silver }, pass: pass, date: date });
+    res.send(`All done, now open the app!`);
 });
+
+app.listen(config.ports.web, () => {
+    console.log(`[INFO] Server ready and listening on port ${config.ports.web}!`);
+});
+
+
 //http://hpmobile.jp/app/nekoatsume/neko_daily_en.php
--- neko.js
+++ neko.js
@@ -2,15 +2,24 @@ const router = require('express').Router();
 
 router.post('/neko_daily_en.php', (req, res) => {
     //1,Password,Silver,Gold,YYYY-MM-DD,
-    res.send('1,Daisy,1000,100,2018-02-24,');
+    let gold = req.cfg.fish.gold;
+    let silver = req.cfg.fish.silver;
+    let date = req.cfg.date;
+    let pass = req.cfg.pass;
+    res.send(`1,${pass},${silver},${gold},${date},`);
+    //res.send('1,Daisy,1000,100,2018-02-24,');
 });
 
 router.post('/neko_aikotoba_en.php', (req, res) => {
     //2,YYYY-MM-DD,Silver,Gold,
     console.log(req.query);
-    let pass = req.query.aiko;
-    if(!pass === 'Daisy') return res.send('0');
-    res.send('2,2018-02-25,1000,100,'); 
+    let passAtt = req.query.aiko;
+    let gold = req.cfg.fish.gold;
+    let silver = req.cfg.fish.silver;
+    let date = req.cfg.date;
+    let pass = req.cfg.pass;
+    res.send(`2,${date},${silver},${gold},`);
+    //res.send('2,2018-02-25,1000,100,'); 
 });
 
 module.exports = router;
